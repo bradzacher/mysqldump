498e6e014343a675824006f845c683aa9443eac7
Brad Zacher <brad.zacher@gmail.com>
2017-12-06 17:25:37 +1100

    added config for running in vscode

---
+++ test/launch.ts
@@ -0,0 +1,16 @@
+import main from '../src/main'
+import testConfig from './testConfig'
+
+// entry point for vs-code launch.json runs
+(async () => {
+    try {
+        const res = await main({
+            connection: testConfig,
+        })
+
+        console.log(res.dump.schema)
+        console.log(res.dump.data)
+    } catch (e) {
+        console.log(e)
+    }
+})()
