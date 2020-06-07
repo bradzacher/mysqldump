b4f0fb1dae7b7c6c99d8f2650c608279f70e2f45
Brad Zacher <brad.zacher@gmail.com>
2020-06-07 12:26:33 -0700

    chore: setup github actions

--- test/testConfig.gh-actions.ts
+++ test/testConfig.gh-actions.ts
@@ -2,7 +2,8 @@ import { ConnectionOptions } from '../src/interfaces/Options';
 
 const config: ConnectionOptions = {
     host: '127.0.0.1',
-    port: 3306,
+    // GH actions assigns a random port that we have to use
+    port: parseInt(process.env.DB_PORT || '3306', 10),
     user: 'root',
     password: '',
     database: 'mysqldump_test',
