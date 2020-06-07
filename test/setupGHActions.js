a2a0948b3b9f82c4c82d4e5bc46b70f070019aa2
Brad Zacher <brad.zacher@gmail.com>
2020-06-07 12:59:31 -0700

    chore: setup github actions (#101)

--- test/setupGHActions.js
+++ test/setupGHActions.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-fs.createReadStream(`${__dirname}/testConfig.travis.ts`).pipe(
+fs.createReadStream(`${__dirname}/testConfig.gh-actions.ts`).pipe(
     fs.createWriteStream(`${__dirname}/testConfig.ts`),
 );
